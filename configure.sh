c62f7d3cf8171ef05c0d76174cf0acac850b5f97
an_geom <93632225+ANGEOM21@users.noreply.github.com>
2024-04-01 11:38:03 +0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+# Installasi paket yang diperlukan
+echo "Memulai instalasi paket..."
 pkg install php php-apache apache2 mariadb -y
+
+# Mengatur konfigurasi PHP dan Apache
+echo "Mengatur konfigurasi..."
 chmod +x conf
 mv -f conf/php $HOME
 rm -r $PREFIX/etc/apache2/httpd.conf
@@ -6,11 +13,18 @@ mv -f conf/httpd.conf $PREFIX/etc/apache2
 mv -f conf/php_module.conf $PREFIX/etc/apache2/extra
 chmod 600 $PREFIX/etc/apache2/httpd.conf
 chmod 644 $PREFIX/etc/apache2/extra/php_module.conf
-unzip $HOME/php/htdocs/phpmyadmin.zip
+
+# Mengekstrak phpMyAdmin
+echo "Mengekstrak phpMyAdmin..."
+unzip $HOME/php/htdocs/phpmyadmin.zip -d $HOME/php/htdocs/
 rm -r $HOME/php/htdocs/phpmyadmin.zip
 chmod 660 $HOME/php/htdocs/phpmyadmin/config.inc.php
 
+# Membersihkan layar
+clear
 
-
-
-
+# Menampilkan pesan bahwa konfigurasi selesai
+echo "Konfigurasi telah selesai!"
+echo "Silakan jalankan perintah berikut untuk menghidupkan layanan:"
+echo "- apachectl untuk menghidupkan Apache"
+echo "- mysqld_safe untuk menghidupkan MySQL"
