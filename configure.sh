8a931cb969f0d41eba2b33ac1eb8d21ebf14353d
Angeom21 <93632225+ANGEOM21@users.noreply.github.com>
2024-04-01 10:59:24 +0700

    update

--- configure.sh
+++ configure.sh
@@ -6,7 +6,9 @@ mv -f conf/httpd.conf $PREFIX/etc/apache2
 mv -f conf/php_module.conf $PREFIX/etc/apache2/extra
 chmod 600 $PREFIX/etc/apache2/httpd.conf
 chmod 644 $PREFIX/etc/apache2/extra/php_module.conf
-chmod 660 php/htdocs/phpmyadmin/config.inc.php
+unzip $HOME/php/htdocs/phpmyadmin.zip
+rm -r $HOME/php/htdocs/phpmyadmin.zip
+chmod 660 $HOME/php/htdocs/phpmyadmin/config.inc.php
 
 
 
